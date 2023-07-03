6e4c8cb7724bc080a2d649dbf31576e322c27d5f
zubetcha <zuhye5@gmail.com>
2023-07-03 17:33:54 +0900

    feat: 현재 설치한 버전이 최신 버전이 아닌 경우 notify

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,57 @@
 #!/usr/bin/env node
 
-import Commander, { program } from 'commander';
+import Commander from 'commander';
+import chalk from 'chalk';
+import { detect } from 'detect-package-manager';
+
+import { getPackageManager } from './lib/package';
 import packageJson from '../package.json';
 
-// const program = new Commander.Command(packageJson.name)
-//   .version(packageJson.version)
-//   .allowUnknownOption()
-//   .parse(process.argv);
+let packageManager = '';
 
-// console.log(program);
+const program = new Commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action(value => console.log(`${chalk.green(getPackageManager())} [options]`))
+  .allowUnknownOption()
+  .parse(process.argv);
 
-// action
-program.action(cmd => console.log('✓ Running!!'));
+// const packageManager = !!program.useNpm
+//   ? 'npm'
+//   : !!program.usePnpm
+//   ? 'pnpm'
+//   : !!program.useYarn
+//   ? 'yarn'
+//   : getPackageManager();
 
-program.parse(process.argv);
+console.log(program);
+
+detect().then(pm => (packageManager = pm));
+
+/**
+ * 현재 버전이 최신 버전이 아닌 경우
+ */
+const notifyUpdate = async () => {
+  const { default: updateNotifier } = await import('update-notifier');
+  const notifier = updateNotifier({ pkg: packageJson });
+
+  if (notifier.update && notifier.update.latest !== packageJson.version) {
+    notifier.notify({
+      defer: false,
+      isGlobal: false,
+      message:
+        `New ${notifier.update.type} version available: ${chalk.dim(
+          '{currentVersion}',
+        )}${chalk.reset(' → ')}${
+          notifier.update.type === 'major'
+            ? chalk.red('{latestVersion}')
+            : notifier.update.type === 'minor'
+            ? chalk.yellow('{latestVersion}')
+            : chalk.green('{latestVersion}')
+        }\n` +
+        `Run ${chalk.cyan('{updateCommand}')} to update\n` +
+        `${chalk.dim.underline(`${packageJson.homepage ?? ''}/releases`)}`,
+    });
+  }
+};
--- src/lib/package.ts
+++ src/lib/package.ts
@@ -1,20 +1,8 @@
 import validate from 'validate-npm-package-name';
+import { promises as fs } from 'fs';
+import { resolve } from 'path';
+import execa from 'execa';
 import type { PackageManager } from '../types/package';
-
-export function getPackageManager(): PackageManager {
-  const npmConfig = process.env.npm_config_user_agent || process.env.npm_execpath || '';
-
-  switch (true) {
-    case npmConfig.includes('yarn'):
-      return 'yarn';
-    case npmConfig.includes('pnpm'):
-      return 'pnpm';
-    case npmConfig.includes('deno'):
-      return 'deno';
-    default:
-      return 'npm';
-  }
-}
 
 export function validatePackageName(name: string): {
   valid: boolean;
@@ -30,4 +18,19 @@ export function validatePackageName(name: string): {
     valid: false,
     problems: [...(nameValidation.errors || []), ...(nameValidation.warnings || [])],
   };
+}
+
+export function getPackageManager(): PackageManager {
+  const npmConfig = process.env.npm_config_user_agent || process.env.npm_execpath || '';
+
+  switch (true) {
+    case npmConfig.includes('yarn'):
+      return 'yarn';
+    case npmConfig.includes('pnpm'):
+      return 'pnpm';
+    case npmConfig.includes('deno'):
+      return 'deno';
+    default:
+      return 'npm';
+  }
 }
