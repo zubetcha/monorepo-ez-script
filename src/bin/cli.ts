7b463b122b88e6a666177c9f3d1ef8225cc2fc46
zubetcha <zuhye5@gmail.com>
2023-07-04 17:14:03 +0900

    feat: init command action

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,6 +1,20 @@
-import { program } from 'commander';
+#!/usr/bin/env node
 
-// action
-program.action(cmd => console.log('✓ Running!!'));
+import { Command } from 'commander';
+import chalk from 'chalk';
+
+import packageJson from '../../package.json';
+import { init } from '../index';
+
+const program = new Command();
+
+program.name(packageJson.name).description(packageJson.description).version(packageJson.version);
+
+program.command('init').description('Create configuration file in root directory').action(init);
+
+program
+  .command('script')
+  .description('Select child package and run script')
+  .action(() => console.log(process.argv));
 
 program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@
 import Commander from 'commander';
 import chalk from 'chalk';
 import { detect } from 'detect-package-manager';
+import path from 'path';
+import fs from 'fs';
+
+import type { Command } from 'commander';
 
 import { getPackageManager } from './lib/package';
 import packageJson from '../package.json';
@@ -24,8 +28,6 @@ const program = new Commander.Command(packageJson.name)
 //   : !!program.useYarn
 //   ? 'yarn'
 //   : getPackageManager();
-
-console.log(program);
 
 detect().then(pm => (packageManager = pm));
 
@@ -54,4 +56,58 @@ const notifyUpdate = async () => {
         `${chalk.dim.underline(`${packageJson.homepage ?? ''}/releases`)}`,
     });
   }
+};
+
+/**
+ * command action: init
+ */
+
+export const init = async (_: unknown, command: Command) => {
+  await notifyUpdate();
+
+  console.log(`mes ${chalk.green(command.name())}`);
+  console.log('Current Working Directory resolved', path.resolve(process.cwd()));
+  console.log('Current Working Directory', process.cwd());
+
+  const cwd = path.resolve(process.cwd());
+
+  // cwd 밖에서 실행되고 있지는 않은지 확인 및 예외처리
+  if (!cwd.startsWith(process.cwd())) {
+    console.error(``);
+    process.exit(1);
+  }
+
+  // 현재 위치가 root인지 확인 및 예외처리
+  // 루트에 git이 있는지 확인
+  if (!fs.existsSync('.git')) {
+    console.error(`Please run CLI at root directory`);
+    process.exit(1);
+  }
+
+  // 현재 위치에 package.json이 있는지 확인 및 예외처리
+  if (!fs.existsSync('package.json')) {
+    console.error(`Please ensure that package.json exists in root directory`);
+    process.exit(1);
+  }
+
+  // package.json에 workspaces 필드가 있는지 확인 및 예외처리
+  const pkgJSON = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+  const lernaJSON = fs.existsSync('lerna.json')
+    ? JSON.parse(fs.readFileSync('lerna.json', 'utf-8'))
+    : {};
+  const workspaces = pkgJSON['workspaces'] || lernaJSON['packages'] || undefined;
+
+  if (!workspaces) {
+    console.error(`Please...`);
+    process.exit(1);
+  }
+
+  // workspace 이름 및 각 workspace package.json의 scripts 정보 수집
+
+  // config 파일 생성
+  const info = {
+    workspaces,
+  };
+
+  fs.writeFileSync('mes.json', JSON.stringify(info));
 };
